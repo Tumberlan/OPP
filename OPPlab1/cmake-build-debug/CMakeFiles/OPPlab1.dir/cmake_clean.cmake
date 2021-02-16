998546dae9b67a0b1022ccfca0a1164893167d0e
i.ashikhmin <i.ashikhmin@g.nsu.ru>
2021-02-16 14:39:36 +0700

    16.02.2021

---
+++ OPPlab1/cmake-build-debug/CMakeFiles/OPPlab1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OPPlab1.dir/main.cpp.obj"
+  "OPPlab1.exe"
+  "OPPlab1.exe.manifest"
+  "OPPlab1.pdb"
+  "libOPPlab1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OPPlab1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
